dc5299070ccb0ec180ce4d92da87fefc296f5a70
Nimmo-san <nat25oloman@gmail.com>
2024-11-07 20:45:36 +0000

    seeding file reformatted with black to satisfy flake8

--- seed.py
+++ seed.py
@@ -8,26 +8,22 @@ mongo_uri = os.getenv("MONGO_URI")
 # print(mongo_uri)
 
 client = MongoClient(mongo_uri)
-db = client['book_db']
-books_collection = db['books']
+db = client["book_db"]
+books_collection = db["books"]
 
 # Data to be inserted (seed data)
 seed_data = [
     {
         "title": "The Catcher in the Rye",
         "author": "J.D. Salinger",
-        "published_date": "1951"
+        "published_date": "1951",
     },
     {
         "title": "To Kill a Mockingbird",
         "author": "Harper Lee",
-        "published_date": "1960"
+        "published_date": "1960",
     },
-    {
-        "title": "1984",
-        "author": "George Orwell",
-        "published_date": "1949"
-    }
+    {"title": "1984", "author": "George Orwell", "published_date": "1949"},
 ]
 
 # Drop existing data if you want a clean slate
